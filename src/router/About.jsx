a13e44c9ab3f31ba1deb46a63cfc55b1228339ff
chanme2000 <chanme2000@naver.com>
2025-10-31 16:24:50 +0900

    yts movie app first project

---
+++ src/router/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='About'>
+      <h2>YTS.It Movie API</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quo ab quas error ea consequatur soluta officia necessitatibus voluptate quisquam qui explicabo nostrum ipsa, quaerat quasi, laudantium magni exercitationem ipsum!
+      Ipsam repellat odit, dolore ea architecto ipsum aliquid nemo error corrupti qui. Officia qui corporis ipsum placeat veritatis. Alias eos officiis aperiam voluptatem odio atque qui. Fugit nemo cum id.</p>
+    </div>
+  )
+}
+
+export default About;
